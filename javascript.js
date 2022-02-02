f1586ac33633e55acaeb83cae9489e4f7a7d5caf
bea <chamoischamois@protonmail.com>
2022-02-02 18:03:34 +0800

    Added sketch functionality and reset button (wip)

--- javascript.js
+++ javascript.js
@@ -1,11 +1,32 @@
 const gridContainer = document.querySelector('#gridContainer');
-    for (let i = 0; i<256; i++){
+
+const resetButton = document.getElementById('resetButton');
+
+let defaultSquares=256;
+
+function makeGrid(){
+
+    for (i = 0; i<defaultSquares; i++){
         let grids = document.createElement('div');
         grids.style.border='1px solid brown';
         grids.style.width= '30px';
         grids.style.height= '30px';
-        grids.style.display='flex';
-        grids.style.flexWrap='wrap';
+        grids.classList.add('square');
         gridContainer.appendChild(grids);
-    };
+
+        const changeColor = () => {
+            grids.style.backgroundColor= 'black';
+        }
+        
+        grids.addEventListener("mouseover", changeColor);
+
+        resetButton.addEventListener("click", ()=> {
+            gridContainer.removeChild(grids);
+            })
+
+    }
+}
+
+makeGrid();
+
 
